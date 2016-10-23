d5be02a5c0b8f43777ddfb28103554396b1f4c93
data-analyser <data.analyser.software@gmail.com>
2016-10-23 20:01:15 +0300

    Added Mongo

--- server/src/main/java/com/fifaminer/infra/Application.java
+++ server/src/main/java/com/fifaminer/infra/Application.java
@@ -2,10 +2,14 @@ package com.fifaminer.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @ComponentScan("com.fifaminer")
 @EnableAutoConfiguration
+@EnableConfigurationProperties
+@EnableMongoRepositories
 public class Application {
 
     public static void main(String[] args) {
