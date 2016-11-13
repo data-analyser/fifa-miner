94a1ef491bc9b91cf392129bae78b74a147e17f2
data-analyser <data.analyser.software@gmail.com>
2016-11-13 13:18:06 +0200

    Fixed name

--- client/src/main/java/com/fifaminer/client/dto/BidStrategy.java
+++ client/src/main/java/com/fifaminer/client/dto/BidStrategy.java
@@ -2,6 +2,6 @@ package com.fifaminer.client.dto;
 
 public final class BidStrategy {
 
-    public static final String MORE_10_THAN_SELL_PRICE_STRATEGY = "more10ThanSellPriceStrategy";
-    public static final String MORE_15_THAN_SELL_PRICE_STRATEGY = "more15ThanSellPriceStrategy";
+    public static final String MORE_10_THAN_SELL_PRICE = "more10ThanSellPriceStrategy";
+    public static final String MORE_15_THAN_SELL_PRICE = "more15ThanSellPriceStrategy";
 }
