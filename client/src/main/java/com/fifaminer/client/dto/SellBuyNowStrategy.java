0145a58022b142c1b0dd9658efd17d74838997ed
data-analyser <data.analyser.software@gmail.com>
2016-11-19 19:14:51 +0200

    Cleared redundant strategies

--- client/src/main/java/com/fifaminer/client/dto/SellBuyNowStrategy.java
+++ client/src/main/java/com/fifaminer/client/dto/SellBuyNowStrategy.java
@@ -3,6 +3,4 @@ package com.fifaminer.client.dto;
 public final class SellBuyNowStrategy {
 
     public static final String FORECASTED_MEDIAN = "forecastedMedianStrategy";
-    public static final String GREATER_15_THAN_CURRENT_MIN = "greater15ThanCurrentMinStrategy";
-    public static final String GREATER_15_THAN_FORECASTED_MIN = "greater15ThanForecastedMinStrategy";
 }
